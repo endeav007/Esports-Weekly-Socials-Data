3de2e995d26bc1c7d0bc5e1e04aa9f59e8af44eb
endeav007 <autumnaustin046@gmail.com>
2025-12-29 16:00:29 -0700

    Removed Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //what will call both instagram and sheets.
 
-import {TestInstagram} from "./instagram.js";
+//import {TestInstagram} from "./instagram.js";
 import {getMedia} from "./instagram.js";
 
 const {INSTAGRAM_MAIN_ID, INSTAGRAM_ACCESS_TOKEN} = process.env;
