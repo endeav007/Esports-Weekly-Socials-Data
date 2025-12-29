838337c4efe55c0703b213f2912722da32bb254c
endeav007 <autumnaustin046@gmail.com>
2025-12-29 15:57:39 -0700

    Added Get Media

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 //what will call both instagram and sheets.
 
 import {TestInstagram} from "./instagram.js";
+import {getMedia} from "./instagram.js";
+
+const {INSTAGRAM_MAIN_ID, INSTAGRAM_ACCESS_TOKEN} = process.env;
 
 async function main(){
     console.log("Fetching basic data");
-    const data = await TestInstagram();
+    //const data = await TestInstagram();
+    const data = getMedia(INSTAGRAM_MAIN_ID, INSTAGRAM_ACCESS_TOKEN);
+    console.log(data);
+
 
 }
 
