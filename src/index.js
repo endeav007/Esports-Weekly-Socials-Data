3e056989b5a1609464e1b20c87a78da6fa6207ad
endeav007 <autumnaustin046@gmail.com>
2026-01-12 12:57:47 -0800

    Finished Account Data Totaling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 //import {TestInstagram} from "./instagram.js";
 //import {getMedia} from "./instagram.js";
-import { getAllAccountData } from "./instagram.js";
+import { runDailyAggregation } from "./instagram.js";
 
 const {INSTAGRAM_MAIN_ID, INSTAGRAM_ACCESS_TOKEN} = process.env;
 
@@ -11,7 +11,7 @@ async function main(){
     //const data = await TestInstagram();
     //const data = getMedia(INSTAGRAM_MAIN_ID, INSTAGRAM_ACCESS_TOKEN);
     //console.log(data);
-    getAllAccountData();
+    runDailyAggregation();
 
 
 }
--- src/instagram.js
+++ src/instagram.js
@@ -5,48 +5,124 @@ import axios from 'axios';
 const { APP_ID, APP_SECRET, INSTAGRAM_ACCESS_TOKEN, INSTAGRAM_MAIN_ID, OVERWATCH_ID, SPLATOON_ID, VALORANT_ID, TEKKEN_ID, 
 MARVELRIVALS_ID, COUNTERSTRIKE_ID, STREETFIGHTER_ID, FORTNITE_ID, SMASH_ID, LEAGUEOFLEGENDS_ID, RAINBOWSIXSIEGE_ID, ROCKETLEAGUE_ID} = process.env;
 
-const all_accounts_data = [];
-const accounts1 ={
-  main: INSTAGRAM_MAIN_ID,
-  overwatch2: OVERWATCH_ID,
-  splatoon3: SPLATOON_ID,
-  valorant: VALORANT_ID,
-  tekken: TEKKEN_ID,
-  marvelrivals: MARVELRIVALS_ID,
-  counterstrike2: COUNTERSTRIKE_ID,
-  streetfighter: STREETFIGHTER_ID,
-  fortnite: FORTNITE_ID,
-  smashultimate: SMASH_ID,
-  leagueoflegends: LEAGUEOFLEGENDS_ID,
-  rainbowsixsiege: RAINBOWSIXSIEGE_ID,
-  rocketleague: ROCKETLEAGUE_ID
-};
+const accounts =[
+  {name: "main", id: INSTAGRAM_MAIN_ID},
+  {name: "overwatch2", id: OVERWATCH_ID},
+  {name: "splatoon", id: SPLATOON_ID},
+  {name: "valorant", id: VALORANT_ID},
+  {name: "tekken", id: TEKKEN_ID},
+  {name: "marvelrivals", id: MARVELRIVALS_ID},
+  {name: "counterstrike2", id: COUNTERSTRIKE_ID},
+  {name: "streetfighter", id: STREETFIGHTER_ID},
+  {name: "fortnite", id: FORTNITE_ID},
+  {name: "smashultimate", id: SMASH_ID},
+  {name: "leagueoflegends", id: LEAGUEOFLEGENDS_ID},
+  {name: "rainbowsixsiege", id: RAINBOWSIXSIEGE_ID},
+  {name: "rocketleague", id : ROCKETLEAGUE_ID}
+];
 
 
-const accounts = [INSTAGRAM_MAIN_ID, OVERWATCH_ID, SPLATOON_ID, VALORANT_ID, TEKKEN_ID, MARVELRIVALS_ID, COUNTERSTRIKE_ID, 
-  STREETFIGHTER_ID, FORTNITE_ID, SMASH_ID, LEAGUEOFLEGENDS_ID, RAINBOWSIXSIEGE_ID, ROCKETLEAGUE_ID]
-
-export async function getMedia(igUserId, pageToken) {
+export async function getMedia(account, pageToken) {
   const res = await axios.get(
-    `https://graph.facebook.com/v18.0/${igUserId}/media`,
+    `https://graph.facebook.com/v18.0/${account.id}/media`,
     {
       params: {
-        fields: "id,caption,media_type,like_count,comments_count,timestamp",
-        limit: 5,
+        fields: "id,media_type,timestamp",
         access_token: pageToken
       }
     }
   );
-  console.log(res.data.data);
-  return res.data.data;
+  return res.data.data.map(media => ({
+    ...media,
+    account_id: account.id,
+    account_name: account.name
+  }));
 }
 
 
 export async function getAllAccountData(){
-  accounts.forEach(account => {
-      all_accounts_data.push(getMedia(account, INSTAGRAM_ACCESS_TOKEN));
-  });
-  console.log(all_accounts_data);
+  
+  const results = await Promise.all(
+    accounts.map(account =>
+      getMedia(account, INSTAGRAM_ACCESS_TOKEN)
+    )
+  );
+  console.log(results.flat());
+  return results.flat();
 }
 
 
+async function fetchMediaWithInsights(mediaArray, token){
+  const recentMedia = mediaArray;
+
+  return await Promise.all(
+    recentMedia.map(async (media) => {
+      try {
+        const insightsResponse = await axios.get(
+          `https://graph.facebook.com/v19.0/${media.id}/insights?metric=likes,comments,shares,total_interactions,views,reposts&access_token=${token}`
+        );
+        media.insights = {};
+        for (let metric of insightsResponse.data.data) {
+          media.insights[metric.name] = metric.values[0].value;
+        }
+      } catch (err) {
+        console.error("Error fetching Instagram data:", err.response?.data || err.message);
+      }
+      return media;
+    })
+  );
+}
+
+function filterMediaSince(mediaArray, sinceDate) {
+  const since = new Date(sinceDate);
+  return mediaArray.filter(media =>
+    new Date(media.timestamp) >= since
+  );
+}
+
+function aggregateMetrics(mediaArray) {
+ const totalsByAccount = {};
+
+  for (const media of mediaArray) {
+    const key = media.account_id;
+
+    if (!totalsByAccount[key]) {
+      totalsByAccount[key] = {
+        account_id: media.account_id,
+        account_name: media.account_name,
+        likes: 0,
+        comments: 0,
+        shares: 0,
+        views: 0,
+        total_interactions: 0
+      };
+    }
+
+    totalsByAccount[key].likes += media.insights?.likes || 0;
+    totalsByAccount[key].comments += media.insights?.comments || 0;
+    totalsByAccount[key].shares += media.insights?.shares || 0;
+    totalsByAccount[key].views += media.insights?.views || 0;
+    totalsByAccount[key].total_interactions += media.insights?.total_interactions || 0;
+  }
+
+  return Object.values(totalsByAccount);
+}
+
+
+
+export async function runDailyAggregation() {
+  const sinceDate = "2024-01-01";
+
+  const allMedia = await getAllAccountData();
+  const filteredMedia = filterMediaSince(allMedia, sinceDate);
+
+  const enrichedMedia = await fetchMediaWithInsights(
+    filteredMedia,
+    INSTAGRAM_ACCESS_TOKEN
+  );
+
+  const totals = aggregateMetrics(enrichedMedia);
+  console.log(totals);
+
+  return totals;
+}
